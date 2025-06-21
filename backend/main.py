8ad45191a3edd2159f7ad9949be4f7ade96c1585
AliSuleman27 <muzamil.ali1099@gmail.com>
2025-06-21 22:24:17 +0500

    main.py--updated

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,17 @@
+import sqlalchemy.orm as orm
+import sqlalchemy as sa
+from sqlalchemy import Column, Integer, String
+
+engine = sa.create_engine("sqlite:///mydb.db")
+Base = orm.declarative_base()   
+sessionLocal = orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
+class User(Base):
+    __tablename__ = "User"
+    name= Column(String,nullable=False)
+    age = Column(Integer,nullable=False)
+
+Base.metadata.create_all(bind=engine)
+
+
+
 
