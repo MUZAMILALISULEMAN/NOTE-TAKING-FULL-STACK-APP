2d418e986b7bdf150775b39450a9e0d7c4df461b
AliSuleman27 <muzamil.ali1099@gmail.com>
2025-07-11 01:27:44 +0500

    deployed

--- backend/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-app = FastAPI()
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://127.0.0.1:5500"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
